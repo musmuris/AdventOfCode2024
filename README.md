11aa217ff0fcde6d3693cda46f5acb4219211544
Nigel Morse <musmuris@users.noreply.github.com>
2024-12-01 21:20:51 +0000

    Day 1

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # AdventOfCode2022
-Hacks for https://adventofcode.com/2024 - trying to learn Rust (from scratch) so might look a bit scrappy and even more so at the start
+Hacks for https://adventofcode.com/2024 - trying to learn Rust (from (almost) scratch - forgot most of it from https://github.com/musmuris/adventOfCode2022) so will look a bit scrappy 
 
 * `scafold 1` to create "day1" setup
 * `watch 1` to run cargo watch to compile and test on changes
--- src/bin/day1.rs
+++ src/bin/day1.rs
@@ -1,7 +1,33 @@
+use std::collections::HashMap;
+
 const INPUT: &str = include_str!("inputs/day1.txt");
 
 pub fn day1(input: &str) -> (usize, usize) {
-    (input.len(), input.len())
+
+    let mut left = Vec::new();
+    let mut right = Vec::new();
+    let mut counts:HashMap<i32,i32> = HashMap::new();
+    for line in input.lines() {
+        let nums = line
+            .split_whitespace()
+            .map(|n| n.parse::<i32>().unwrap())
+            .collect::<Vec<_>>();
+        left.push(nums[0]);
+        right.push(nums[1]);
+        *counts.entry(nums[1]).or_insert(0) += 1;        
+    }
+    left.sort();
+    right.sort();
+
+    let nums = left.iter().zip(right.iter());
+    let mut distance: i32 = 0;
+    let mut similarity: i32 = 0;
+    for (lnum, rnum) in nums {
+        distance += (lnum - rnum).abs();
+        similarity += lnum * counts.get(lnum).unwrap_or(&0);
+    }
+
+    (distance as usize, similarity as usize)
 }
 
 fn main() {
@@ -18,15 +44,15 @@ mod tests {
         let input = include_str!("inputs/day1.test1.txt");
         let (p1, p2) = day1(input);
 
-        assert_eq!(p1, input.len());
-        assert_eq!(p2, input.len());
+        assert_eq!(p1, 11);
+        assert_eq!(p2, 31);
     }
 
     #[test]
     fn test_main() {
         let (p1, p2) = day1(INPUT);
 
-        assert_eq!(p1, INPUT.len());
-        assert_eq!(p2, INPUT.len());
+        assert_eq!(p1, 1197984);
+        assert_eq!(p2, 23387399);
     }
 }
